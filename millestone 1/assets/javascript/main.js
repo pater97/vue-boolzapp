935911d52d6a27a12417e707fdb141b0fb136fde
pater97 <giuseppepater997@gmail.com>
2021-11-03 13:23:49 +0100

    millestone 1 completo. BUG:l'immagine di sfondo non compare

--- millestone 1/assets/javascript/main.js
+++ millestone 1/assets/javascript/main.js
@@ -6,7 +6,6 @@ dall’interlocutore (bianco) assegnando due classi CSS diverse
 ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 nome e immagine di ogni contatto.
 */
-
 const app = new Vue({
     el:`#app`,
     data:{
